732920e29e19a23f4adb96f653fed3de4579125f
90zbear <pepper@stupid.jp>
2014-02-06 18:43:56 +0900

    add dummy text function

--- jq.htmlmockhelper.js
+++ jq.htmlmockhelper.js
@@ -2,12 +2,17 @@
 	$.fn.htmlmockhelper = function( options ){
 		// default
 		var defaults = {
-			"mode" : "repeat",
-			"repeat" : 10
+			"mode" : "none",
+			"repeat" : 10,
+			"text" : 200,
+			"textstr" : "this is dummy text. "
 		};
 
 		var setting = $.extend( defaults, options );
+		// none
+		if( setting['mode'] == "none" ){}
 
+		// repeat
 		if( setting['mode'] == "repeat" ){
 			$(this).find(".copyobj").each(function(){
 				for(var i = 1; i< setting['repeat']; i++){
@@ -15,5 +20,26 @@
 				}
 			});
 		}
+		// dummy text
+		if( setting['mode'] == "text" ){
+			$(this).find(".dummy").each(function(){
+				var defaultDummy = setting['textstr'];
+				var defCount = defaultDummy.length;
+				var repeatText = Math.floor(setting['text'] / defCount) + 2;
+
+				var strText = new String();
+				for (var i = 0; i < repeatText; i++) {
+					strText += defaultDummy;
+				};
+				var txt = new String();
+				if ( setting['text'] > defaultDummy.length ){
+					txt = strText.substr( 0, setting['text'] );
+				}else{
+					txt = strText;
+				}
+				$(this).html(txt);
+			});
+		}
+
 	}
 })(jQuery);
